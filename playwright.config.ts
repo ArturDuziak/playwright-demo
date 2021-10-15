ff1b5ae0fc35cf19122b360bf4b0e570648ff4c8
Artur Duziak <arturduziak97@gmail.com>
2021-10-15 22:59:05 +0200

    Add initial Playwright configuration and test

---
+++ playwright.config.ts
@@ -0,0 +1,58 @@
+import { PlaywrightTestConfig, devices } from "@playwright/test";
+import path from "path";
+
+// Reference: https://playwright.dev/docs/test-configuration
+const config: PlaywrightTestConfig = {
+  timeout: 30 * 1000,
+  testDir: path.join(__dirname, "playwright/tests"),
+  retries: 0,
+  outputDir: "test-results/",
+
+  // Run your local dev server before starting the tests:
+  // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
+  // webServer: {
+  //   command: "npm run start",
+  //   port: 3000,
+  // },
+
+  use: {
+    trace: "retry-with-trace",
+    contextOptions: {
+      ignoreHTTPSErrors: true,
+    },
+    baseURL: "http://localhost:3000",
+    headless: false,
+  },
+
+  projects: [
+    {
+      name: "Desktop Chrome",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+    },
+    // {
+    //   name: "Desktop Firefox",
+    //   use: {
+    //     ...devices["Desktop Firefox"],
+    //   },
+    // },
+    // {
+    //   name: "Desktop Safari",
+    //   use: {
+    //     ...devices["Desktop Safari"],
+    //   },
+    // },
+    // {
+    //   name: "Mobile Chrome",
+    //   use: {
+    //     ...devices["Pixel 5"],
+    //   },
+    // },
+    // {
+    //   name: "Mobile Safari",
+    //   use: devices["iPhone 12"],
+    // },
+  ],
+};
+export default config;
