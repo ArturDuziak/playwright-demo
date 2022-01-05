364f43fd87d785b2d7131597046f9ec5b679506e
Artur Duziak <arturduziak97@gmail.com>
2022-01-05 09:53:57 +0100

    test: Skip clipboard test on CI environment

--- playwright/tests/clipboard.spec.ts
+++ playwright/tests/clipboard.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "@playwright/test";
 import clipboard from 'clipboardy';
 
+test.fixme(!!process.env.CI, 'There is an issue with xsel binary when running the test on CI');
 test("Using external libraries you can test copy to clipboard features", async ({ page }) => {
   await page.goto('https://clipboardjs.com/');
   await page.click('#example-target button');
