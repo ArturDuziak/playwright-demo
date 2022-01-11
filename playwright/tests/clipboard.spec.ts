134e51c3420c5b0fdf52b58c1298a88b28ceb777
Artur Duziak <arturduziak97@gmail.com>
2022-01-11 09:22:44 +0100

    test: Update clipboard example

--- playwright/tests/clipboard.spec.ts
+++ playwright/tests/clipboard.spec.ts
@@ -1,12 +1,13 @@
 import { expect, test } from "@playwright/test";
-import clipboard from 'clipboardy';
 
-test.fixme(!!process.env.CI, 'There is an issue with xsel binary when running the test on CI');
-test("Using external libraries you can test copy to clipboard features", async ({ page }) => {
+test("Using evaluate you can access clipboard value on chrome", async ({ browserName, page, context }) => {
+  test.skip(browserName !== 'chromium', 'You can easily access clipboard only on chrome browser');
+
+  await context.grantPermissions(['clipboard-read']);
+
   await page.goto('https://clipboardjs.com/');
   await page.click('#example-target button');
 
-  const copiedText = await clipboard.read();
-  expect(copiedText).toBe('https://github.com/zenorocha/clipboard.js.git');
+  expect(await page.evaluate(() => navigator.clipboard.readText())).toBe('https://github.com/zenorocha/clipboard.js.git');
 });
 
