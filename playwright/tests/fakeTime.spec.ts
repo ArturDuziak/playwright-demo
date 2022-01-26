42d8cc3dd254c351b6640c204179348fd58b8688
Artur Duziak <arturduziak97@gmail.com>
2022-01-26 09:17:38 +0100

    test: Add time controlling tests

---
+++ playwright/tests/fakeTime.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+import path from 'path';
+
+test('Controlling clock using external package', async ({ context, page }) => {
+  await context.addInitScript({
+    path: path.join(__dirname, '../../', './node_modules/sinon/pkg/sinon.js'),
+  });
+  await context.addInitScript(() => {
+    // @ts-expect-error
+    window.__clock = sinon.useFakeTimers();
+  });
+
+  page.goto('http://time-time.net/timer/digital-clock.php');
+
+  const timer = page.locator('#timenow');
+
+  await expect(timer).toContainText('1:00:00');
+  // @ts-expect-error
+  await page.evaluate(() => window.__clock.tick(60 * 60 * 1000));
+  await expect(timer).toContainText('2:00:00');
+});
