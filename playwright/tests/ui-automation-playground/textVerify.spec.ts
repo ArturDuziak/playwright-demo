78e2449d62cfc97aa30c430fef6773e474363742
Artur Duziak <arturduziak97@gmail.com>
2022-03-05 13:48:45 +0100

    feat: Update Playwright version to 1.19.2

--- playwright/tests/ui-automation-playground/textVerify.spec.ts
+++ playwright/tests/ui-automation-playground/textVerify.spec.ts
@@ -3,6 +3,5 @@ import { test, expect } from "@playwright/test";
 test("Verifies text", async ({ page }) => {
   await page.goto("http://www.uitestingplayground.com/verifytext");
 
-  const text = await page.isVisible("text=Welcome");
-  expect(text).toBeTruthy();
+  await expect(page.locator(".bg-primary")).toHaveText("Welcome UserName!");
 });
