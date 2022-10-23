09729b3ec6e0f9464a5bdb5a0791b2b819886cb8
ArturDuziak <arturduziak97@gmail.com>
2022-10-23 20:43:51 +0200

    feat: Adjust flaky test

--- playwright/tests/trello-app/signup.spec.ts
+++ playwright/tests/trello-app/signup.spec.ts
@@ -6,43 +6,46 @@ import faker from "faker";
 
 let loginPage: LoginPage;
 
-test.beforeEach(async ({ page }) => {
-  loginPage = new LoginPage(page);
+test.describe("Sign up page", () => {
+  test.skip(({ browserName }) => browserName == "webkit", "Tests are flaky on webkit");
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
 
-  await page.goto("/");
+    await page.goto("/");
 
-  await loginPage.openLoginMenu();
-  await page.click("text=Sign up here");
-});
+    await loginPage.openLoginMenu();
+    await page.click("text=Sign up here");
+  });
 
-test("Sign Up modal displays correctly @visual", async ({ page }) => {
-  await expect(loginPage.signupButton).toBeVisible();
+  test("Sign Up modal displays correctly @visual", async ({ page }) => {
+    await expect(loginPage.signupButton).toBeVisible();
 
-  await percySnapshot(page, "Sign Up Modal");
-});
+    await percySnapshot(page, "Sign Up Modal");
+  });
 
-test("User can successfully sign up", async () => {
-  const email = faker.internet.email();
-  const password = faker.internet.password();
+  test("User can successfully sign up", async () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
 
-  await loginPage.signUp(email, password);
+    await loginPage.signUp(email, password);
 
-  await expect(loginPage.loginModal).not.toBeVisible();
-  await expect(loginPage.loggedInBanner).toBeVisible();
-  await expect(loginPage.userMenu).toContainText(email);
-});
+    await expect(loginPage.loginModal).not.toBeVisible();
+    await expect(loginPage.loggedInBanner).toBeVisible();
+    await expect(loginPage.userMenu).toContainText(email);
+  });
 
-test("Existing user cannot sign up", async ({ page }) => {
-  const email = faker.internet.email();
-  const password = faker.internet.password();
+  test("Existing user cannot sign up", async ({ page }) => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
 
-  createUser(page, { email, password });
+    createUser(page, { email, password });
 
-  const [response] = await Promise.all([
-    page.waitForResponse("**/signup"),
-    loginPage.signUp(email, password),
-  ]);
+    const [response] = await Promise.all([
+      page.waitForResponse("**/signup"),
+      loginPage.signUp(email, password),
+    ]);
 
-  await expect(response.status()).toBe(400);
-  await expect(loginPage.loginModal).toBeVisible();
+    expect(response.status()).toBe(400);
+    await expect(loginPage.loginModal).toBeVisible();
+  });
 });
